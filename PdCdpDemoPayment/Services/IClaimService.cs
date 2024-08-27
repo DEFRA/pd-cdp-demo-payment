82aa9dc3cf6cdbb1737deb6995515c79526d021d
John Watson <john.watson1@defra.gov.uk>
2024-08-27 15:05:36 +0100

    Initial setup

---
+++ PdCdpDemoPayment/Services/IClaimService.cs
@@ -0,0 +1,10 @@
+﻿using PdCdpDemoPayment.Models;
+
+namespace PdCdpDemoPayment.Services;
+
+public interface IClaimService
+{
+    public Task<bool> CreateAsync(Claim book);
+
+    public Task<Claim?> GetByClaimIdAsync(string claimId);
+}
---
+++ PdCdpDemoPayment/Validators/ClaimValidator.cs
@@ -0,0 +1,16 @@
+﻿using PdCdpDemoPayment.Models;
+using FluentValidation;
+
+namespace PdCdpDemoPayment.Validators;
+
+public class ClaimValidator : AbstractValidator<Claim>
+{
+    public ClaimValidator()
+    {
+        RuleFor(claim => claim.ClaimId)
+            .Matches(@"^MINE\d{3}$")
+            .WithMessage("Value was not a valid claimId");
+
+        RuleFor(claim => claim.Name).NotEmpty();
+    }
+}
